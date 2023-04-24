1b152a97b0ab670b05cdd555635609a1acf1ccf1
TienPerry <52100124@student.tdtu.edu.vn>
2023-04-24 10:28:16 +0700

    update

---
+++ DrugStore/DrugStore/FAddNCC.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace DrugStore
+{
+    partial class FAddNCC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FAddNCC));
+            this.pb_luu = new System.Windows.Forms.PictureBox();
+            this.pb_huy = new System.Windows.Forms.PictureBox();
+            this.tb_mancc = new System.Windows.Forms.TextBox();
+            this.pb_thongtinncc = new System.Windows.Forms.PictureBox();
+            this.tb_tenncc = new System.Windows.Forms.TextBox();
+            this.tb_sdtncc = new System.Windows.Forms.TextBox();
+            this.tb_diachincc = new System.Windows.Forms.TextBox();
+            this.lb_mancc = new System.Windows.Forms.Label();
+            this.lb_tenncc = new System.Windows.Forms.Label();
+            this.lb_sdtncc = new System.Windows.Forms.Label();
+            this.lb_diachincc = new System.Windows.Forms.Label();
+            this.pnl_mancc = new System.Windows.Forms.Panel();
+            this.pnl_tenncc = new System.Windows.Forms.Panel();
+            this.pnl_sdtncc = new System.Windows.Forms.Panel();
+            this.pnl_diachincc = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_luu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_huy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thongtinncc)).BeginInit();
+            this.pnl_mancc.SuspendLayout();
+            this.pnl_tenncc.SuspendLayout();
+            this.pnl_sdtncc.SuspendLayout();
+            this.pnl_diachincc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_luu
+            // 
+            this.pb_luu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_luu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb_luu.Image = ((System.Drawing.Image)(resources.GetObject("pb_luu.Image")));
+            this.pb_luu.Location = new System.Drawing.Point(644, 573);
+            this.pb_luu.Name = "pb_luu";
+            this.pb_luu.Size = new System.Drawing.Size(153, 51);
+            this.pb_luu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_luu.TabIndex = 1;
+            this.pb_luu.TabStop = false;
+            this.pb_luu.Click += new System.EventHandler(this.pb_luu_Click);
+            // 
+            // pb_huy
+            // 
+            this.pb_huy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_huy.Image = ((System.Drawing.Image)(resources.GetObject("pb_huy.Image")));
+            this.pb_huy.Location = new System.Drawing.Point(836, 573);
+            this.pb_huy.Name = "pb_huy";
+            this.pb_huy.Size = new System.Drawing.Size(153, 51);
+            this.pb_huy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_huy.TabIndex = 2;
+            this.pb_huy.TabStop = false;
+            this.pb_huy.Click += new System.EventHandler(this.pb_huy_Click);
+            // 
+            // tb_mancc
+            // 
+            this.tb_mancc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(240)))), ((int)(((byte)(247)))));
+            this.tb_mancc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_mancc.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_mancc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tb_mancc.Location = new System.Drawing.Point(0, 0);
+            this.tb_mancc.Name = "tb_mancc";
+            this.tb_mancc.Size = new System.Drawing.Size(319, 31);
+            this.tb_mancc.TabIndex = 3;
+            // 
+            // pb_thongtinncc
+            // 
+            this.pb_thongtinncc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb_thongtinncc.Image = ((System.Drawing.Image)(resources.GetObject("pb_thongtinncc.Image")));
+            this.pb_thongtinncc.Location = new System.Drawing.Point(0, 0);
+            this.pb_thongtinncc.Name = "pb_thongtinncc";
+            this.pb_thongtinncc.Size = new System.Drawing.Size(1010, 653);
+            this.pb_thongtinncc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_thongtinncc.TabIndex = 0;
+            this.pb_thongtinncc.TabStop = false;
+            // 
+            // tb_tenncc
+            // 
+            this.tb_tenncc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(240)))), ((int)(((byte)(247)))));
+            this.tb_tenncc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_tenncc.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_tenncc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tb_tenncc.Location = new System.Drawing.Point(0, 0);
+            this.tb_tenncc.Name = "tb_tenncc";
+            this.tb_tenncc.Size = new System.Drawing.Size(361, 31);
+            this.tb_tenncc.TabIndex = 4;
+            // 
+            // tb_sdtncc
+            // 
+            this.tb_sdtncc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(240)))), ((int)(((byte)(247)))));
+            this.tb_sdtncc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_sdtncc.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_sdtncc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tb_sdtncc.Location = new System.Drawing.Point(0, 0);
+            this.tb_sdtncc.Name = "tb_sdtncc";
+            this.tb_sdtncc.Size = new System.Drawing.Size(361, 31);
+            this.tb_sdtncc.TabIndex = 5;
+            // 
+            // tb_diachincc
+            // 
+            this.tb_diachincc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(240)))), ((int)(((byte)(247)))));
+            this.tb_diachincc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_diachincc.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_diachincc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tb_diachincc.Location = new System.Drawing.Point(0, 0);
+            this.tb_diachincc.Name = "tb_diachincc";
+            this.tb_diachincc.Size = new System.Drawing.Size(367, 31);
+            this.tb_diachincc.TabIndex = 6;
+            // 
+            // lb_mancc
+            // 
+            this.lb_mancc.AutoSize = true;
+            this.lb_mancc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.lb_mancc.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_mancc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(255)))), ((int)(((byte)(114)))));
+            this.lb_mancc.Location = new System.Drawing.Point(81, 179);
+            this.lb_mancc.Name = "lb_mancc";
+            this.lb_mancc.Size = new System.Drawing.Size(249, 38);
+            this.lb_mancc.TabIndex = 7;
+            this.lb_mancc.Text = "Mã nhà cung cấp:";
+            // 
+            // lb_tenncc
+            // 
+            this.lb_tenncc.AutoSize = true;
+            this.lb_tenncc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.lb_tenncc.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_tenncc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(255)))), ((int)(((byte)(114)))));
+            this.lb_tenncc.Location = new System.Drawing.Point(81, 275);
+            this.lb_tenncc.Name = "lb_tenncc";
+            this.lb_tenncc.Size = new System.Drawing.Size(252, 38);
+            this.lb_tenncc.TabIndex = 8;
+            this.lb_tenncc.Text = "Tên nhà cung cấp:";
+            // 
+            // lb_sdtncc
+            // 
+            this.lb_sdtncc.AutoSize = true;
+            this.lb_sdtncc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.lb_sdtncc.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_sdtncc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(255)))), ((int)(((byte)(114)))));
+            this.lb_sdtncc.Location = new System.Drawing.Point(130, 372);
+            this.lb_sdtncc.Name = "lb_sdtncc";
+            this.lb_sdtncc.Size = new System.Drawing.Size(200, 38);
+            this.lb_sdtncc.TabIndex = 9;
+            this.lb_sdtncc.Text = "Số điện thoại:";
+            // 
+            // lb_diachincc
+            // 
+            this.lb_diachincc.AutoSize = true;
+            this.lb_diachincc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.lb_diachincc.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_diachincc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(255)))), ((int)(((byte)(114)))));
+            this.lb_diachincc.Location = new System.Drawing.Point(215, 473);
+            this.lb_diachincc.Name = "lb_diachincc";
+            this.lb_diachincc.Size = new System.Drawing.Size(115, 38);
+            this.lb_diachincc.TabIndex = 10;
+            this.lb_diachincc.Text = "Địa chỉ:";
+            // 
+            // pnl_mancc
+            // 
+            this.pnl_mancc.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pnl_mancc.Controls.Add(this.tb_mancc);
+            this.pnl_mancc.Location = new System.Drawing.Point(376, 184);
+            this.pnl_mancc.Name = "pnl_mancc";
+            this.pnl_mancc.Size = new System.Drawing.Size(319, 38);
+            this.pnl_mancc.TabIndex = 11;
+            // 
+            // pnl_tenncc
+            // 
+            this.pnl_tenncc.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pnl_tenncc.Controls.Add(this.tb_tenncc);
+            this.pnl_tenncc.Location = new System.Drawing.Point(376, 282);
+            this.pnl_tenncc.Name = "pnl_tenncc";
+            this.pnl_tenncc.Size = new System.Drawing.Size(361, 38);
+            this.pnl_tenncc.TabIndex = 12;
+            // 
+            // pnl_sdtncc
+            // 
+            this.pnl_sdtncc.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pnl_sdtncc.Controls.Add(this.tb_sdtncc);
+            this.pnl_sdtncc.Location = new System.Drawing.Point(376, 379);
+            this.pnl_sdtncc.Name = "pnl_sdtncc";
+            this.pnl_sdtncc.Size = new System.Drawing.Size(361, 38);
+            this.pnl_sdtncc.TabIndex = 13;
+            // 
+            // pnl_diachincc
+            // 
+            this.pnl_diachincc.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pnl_diachincc.Controls.Add(this.tb_diachincc);
+            this.pnl_diachincc.Location = new System.Drawing.Point(376, 478);
+            this.pnl_diachincc.Name = "pnl_diachincc";
+            this.pnl_diachincc.Size = new System.Drawing.Size(367, 38);
+            this.pnl_diachincc.TabIndex = 14;
+            // 
+            // FAddNCC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 653);
+            this.Controls.Add(this.lb_mancc);
+            this.Controls.Add(this.lb_tenncc);
+            this.Controls.Add(this.lb_sdtncc);
+            this.Controls.Add(this.lb_diachincc);
+            this.Controls.Add(this.pnl_mancc);
+            this.Controls.Add(this.pnl_tenncc);
+            this.Controls.Add(this.pnl_sdtncc);
+            this.Controls.Add(this.pnl_diachincc);
+            this.Controls.Add(this.pb_huy);
+            this.Controls.Add(this.pb_luu);
+            this.Controls.Add(this.pb_thongtinncc);
+            this.Name = "FAddNCC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddNCC";
+            this.Load += new System.EventHandler(this.FAddNCC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_luu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_huy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thongtinncc)).EndInit();
+            this.pnl_mancc.ResumeLayout(false);
+            this.pnl_mancc.PerformLayout();
+            this.pnl_tenncc.ResumeLayout(false);
+            this.pnl_tenncc.PerformLayout();
+            this.pnl_sdtncc.ResumeLayout(false);
+            this.pnl_sdtncc.PerformLayout();
+            this.pnl_diachincc.ResumeLayout(false);
+            this.pnl_diachincc.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private PictureBox pb_luu;
+        private PictureBox pb_huy;
+        private TextBox tb_mancc;
+        private PictureBox pb_thongtinncc;
+        private TextBox tb_tenncc;
+        private TextBox tb_sdtncc;
+        private TextBox tb_diachincc;
+        private Label lb_mancc;
+        private Label lb_tenncc;
+        private Label lb_sdtncc;
+        private Label lb_diachincc;
+        private Panel pnl_mancc;
+        private Panel pnl_tenncc;
+        private Panel pnl_sdtncc;
+        private Panel pnl_diachincc;
+    }
+}
